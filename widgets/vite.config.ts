8eb3edb62de87d21c29717bef215b0dd35c67ac2
auddnjs2008 <auddnjs2008@naver.com>
2026-05-30 14:06:19 +0900

    fix cps test

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const FULL_URL = "https://readingdeck.auddnjs2008.workers.dev/";
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), tailwindcss()],
-    base: mode === "production" ? "./" : undefined,
+    base: mode === "production" ? FULL_URL : undefined,
     build: {
       outDir: "../server/dist",
       emptyOutDir: true,
